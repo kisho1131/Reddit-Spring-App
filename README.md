4a23c4d2dbb364baeb2d0fa2277f72168529a02b
Sumit Kumar <sumitkishore0131@gmail.com>
2022-05-28 21:48:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Reddit-Spring-App
+ Reddit  webapplication using SpringBoot as backend framework and Angular as frontend
---
+++ src/test/java/com/redditapp/RedditSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redditapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
